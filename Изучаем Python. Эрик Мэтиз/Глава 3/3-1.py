06a183d318126c3dadf9f6cf39f6a3e461cd66df
varlog762 <boot762@gmail.com>
2019-10-02 21:02:43 +0300

    Added file name.py.

---
+++ Изучаем Python. Эрик Мэтиз/Глава 3/3-1.py
@@ -0,0 +1,4 @@
+name = ["Eugeniya", "Olga", "Andrew"]
+print(name[0])
+print(name[1])
+print(name[2])
---
+++ Изучаем Python. Эрик Мэтиз/Глава 3/3-2.py
@@ -0,0 +1,6 @@
+#Gregory Pereverzev 02.10.2019
+
+name = ["Eugeniya", "Olga", "Andrew"]
+print("Hello " + name[0] + "!")
+print("Hello " + name[1] + "!")
+print("Hello " + name[2] + "!")
