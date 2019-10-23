505c50c497ab4382169d318da10b2de146d73dfb
varlog762 <boot762@gmail.com>
2019-10-23 13:20:59 +0300

    Added file name.py.

---
+++ Изучаем Python. Эрик Мэтиз/Глава 3/3 - test.py
@@ -0,0 +1,8 @@
+#Gregory Pereverzev 02.10.2019
+
+test = [1, 2, 3]
+print(test)
+test.insert(8, 111)
+test.insert(10, "dsccj")
+print(test)
+
---
+++ Изучаем Python. Эрик Мэтиз/Глава 3/3-3.py
@@ -0,0 +1,6 @@
+#Gregory Pereverzev 02.10.2019
+
+cars = ["Honda", "Nissan", "BMW"]
+print("I want buy " + cars[0] + "!")
+print("I want buy " + cars[1] + "!")
+print("I want buy " + cars[2] + "!")
