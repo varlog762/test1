7ccfbc0d05afc46d4f60582351b3b6c1e7b73645
varlog762 <boot762@gmail.com>
2019-10-23 16:36:34 +0300

    Added file name.py.

---
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-6.py
@@ -0,0 +1,16 @@
+#Gregory Pereverzev 23.10.2019
+
+age = 33
+
+if age < 2:
+    print("Mladenets")
+elif age >= 2 and age < 4:
+    print("Malysh")
+elif age >= 4 and age < 13:
+    print("keed")
+elif age >= 13 and age < 20:
+    print("Teenager")
+elif age >= 20 and age < 65:
+    print("Man")
+elif age >= 65:
+    print("Old man")
---
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-7.py
@@ -0,0 +1,14 @@
+#Gregory Pereverzev 23.10.2019
+
+favorite_fruits = ["apple", "bananas", "peach"]
+
+if "apple" in favorite_fruits:
+    print("Yes, apple!")
+if "plum" in favorite_fruits:
+    print("Yes, plum!")
+if "cherry" in favorite_fruits:
+    print("Yes, cherry!")
+if "peach" in favorite_fruits:
+    print("Yes, peach!")
+if "bananas" in favorite_fruits:
+    print("Yes, bananas!")
