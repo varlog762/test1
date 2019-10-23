29560bd4e6012cfc3037c6fe3032eb3b2edfb4d9
varlog762 <boot762@gmail.com>
2019-10-23 13:44:02 +0300

    Added file name.py.

---
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-1.py
@@ -0,0 +1,22 @@
+#Gregory Pereverzev 23.10.2019
+
+cat = "August"
+print("If cat == 'August', it's GOOD!")
+if cat == "August":
+    print("GOOD!")
+else:
+    print(cat == "Cheburashka")
+print(cat == "cheburashka")
+
+work = "Google"
+print("\nIf work == 'Google', i keep True")
+print(work == "Google")
+print("Else i keep False")
+print(work == "Apple")
+
+wife = "Eugenia"
+print("\nIf wife == 'Eugenia' it's True")
+print(wife.upper() == "EUGENIA")
+print("Else this False!")
+print(wife.lower() == "HUIhjmoi")
+
