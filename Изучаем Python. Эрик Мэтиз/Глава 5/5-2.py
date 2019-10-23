fc5ef9ec5b79583d42b73e582ff3463a78f2de91
varlog762 <boot762@gmail.com>
2019-10-23 15:48:51 +0300

    Added file name.py.

--- Изучаем Python. Эрик Мэтиз/Глава 5/5-2.py
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-2.py
@@ -4,4 +4,27 @@ list0 = ["Eugenia", "Andrew", "Slavka", "Tolik"]
 list1 = [1, 34, 435, 2, 7]
 cat = "August"
 
-print("If cat == 'August'
+print("If cat == 'August', it's good!")
+if cat.lower() == "august":
+    print('Good!')
+print("If cat != 'August', it's bad!")
+if cat != "udhvduivn":
+    print("Bad!")
+
+if "Eugenia" in list0:
+    print("\nGood!")
+else:
+    print("Bad!")
+
+if list1[1] > list1[3]:
+    print("True")
+else:
+    print("False")
+
+if 15 >= list1[0] and 32 <= list1[4]:
+    print("False")
+else:
+    print("True")
+
+if "Shura" not in list0:
+    print("True")
---
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-3.py
@@ -0,0 +1,8 @@
+#Gregory Pereverzev 23.10.2019
+
+alien_color = "red"
+if alien_color == "green":
+    print("Kill him")
+
+if alien_color == "red":
+    print("He's dead!")
