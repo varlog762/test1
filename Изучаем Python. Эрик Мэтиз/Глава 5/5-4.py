678c70ddba6de385911c9907f58aa57fa656db94
varlog762 <boot762@gmail.com>
2019-10-23 16:17:31 +0300

    Added file name.py.

---
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-4.py
@@ -0,0 +1,13 @@
+#Gregory Pereverzev 23.10.2019
+
+alien_color = "red"
+if alien_color == "green":
+    print("You get 5 points!")
+else:
+    print("You get 10 points!")
+
+alien_color1 = "green"
+if alien_color1 == "green":
+    print("You get 5 points!")
+else:
+    print("You get 10 points!")
---
+++ Изучаем Python. Эрик Мэтиз/Глава 5/5-5.py
@@ -0,0 +1,25 @@
+#Gregory Pereverzev 23.10.2019
+
+alien_color = "green"
+if alien_color == "green":
+    print("You get 5 points!")
+elif alien_color == "red":
+    print("You get 10 points!")
+elif alien_color == "yellow":
+    print("You get 15 points!")
+
+alien_color = "red"
+if alien_color == "green":
+    print("You get 5 points!")
+elif alien_color == "red":
+    print("You get 10 points")
+elif alien_color == "yellow":
+    print("You get 15 points!")
+
+alien_color = "yellow"
+if alien_color == "green":
+    print("You get 5 points")
+elif alien_color == "red":
+    print("You get 10 points")
+elif alien_color == "yellow":
+    print("You get 15 points")
