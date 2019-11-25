c1088fddd40b49d7c7bf0b5bff5c4343b1c4d5bd
Gregory Pereverzev <boot762@gmail.com>
2019-11-25 18:36:28 +0300

    Создаем первый модуль.

---
+++ Head First Python. Paul Barry/mymodules/setup.py
@@ -0,0 +1,11 @@
+#Gregory Pereverzev 25.11.2019
+
+from setuptools import setup
+
+setup(name = 'vsearch',
+      version = '1.0',
+      description = 'The Head First Python Search Tools',
+      author = 'Gregory Pereverzev',
+      author_email = 'boot762@gmail.com',
+      url = 'headfirstlab.com',
+      py_modules = ['vsearch'])
