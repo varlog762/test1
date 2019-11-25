5484fafeb5717324443bb9ca2d47db7e40c72d69
Gregory Pereverzev <boot762@gmail.com>
2019-11-25 17:31:05 +0300

    Создаем первые функции.

---
+++ Head First Python. Paul Barry/4-1 vsearch.py
@@ -0,0 +1,14 @@
+#Gregory Pereverzev 25.11.2019
+#Создаем первые функции.
+
+def search4vowels(phrase:str) -> set:
+    '''Возвращает гласные, найденные в указанной фразе.'''
+    vowels = ser('aeiou')
+    return vowels.intersection(set(phrase))
+
+
+def search4letters(phrase:str, letters:str) -> set:
+    ''''''
+    return set(letters).intersection(set(phrase))
+
+#print(search4letters('fbviweo', 'v'))
