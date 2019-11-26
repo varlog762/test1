297ab201b34726833cfeae64760f576a0b7786f9
Gregory Pereverzev <boot762@gmail.com>
2019-11-26 17:55:14 +0300

    ...

---
+++ Head First Python. Paul Barry/mymodules/mystery.py
@@ -0,0 +1,11 @@
+#Gregory Pereverzev 26.11.2019
+
+def double(arg):
+    print('Before: ', arg)
+    arg = arg * 2
+    print('After: ', arg)
+
+def change(arg):
+    print('Before: ', arg)
+    arg.append('More data')
+    print('After: ', arg)
--- Head First Python. Paul Barry/mymodules/vsearch.py
+++ Head First Python. Paul Barry/mymodules/vsearch.py
@@ -12,4 +12,3 @@ def search4letters(phrase:str, letters=str('aeiou')) -> set:
     '''Возвращает множество букв из letters, найденных в указанной фразе.'''
     return set(letters).intersection(set(phrase))
 
-print(search4letters('fbviweo', 'v'))
