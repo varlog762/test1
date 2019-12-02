6c4821a8efa2c5f31ba4cacd77a19a6411b5790a
varlog762 <boot762@gmail.com>
2019-12-02 15:25:24 +0300

    Доработано веб-приложение vsearch.

---
+++ Head First Python. Paul Barry/webapp/vsearch.py
@@ -0,0 +1,14 @@
+'''Gregory Pereverzev 25.11.2019
+Создаем первые функции.'''
+
+
+def search4vowels(phrase: str) -> set:
+    '''Возвращает гласные, найденные в указанной фразе.'''
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters=str('aeiou')) -> set:
+    '''Аргументу letters присвоено значение по умолчанию.'''
+    '''Возвращает множество букв из letters, найденных в указанной фразе.'''
+    return set(letters).intersection(set(phrase))
